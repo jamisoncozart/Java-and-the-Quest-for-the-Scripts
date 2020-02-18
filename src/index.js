f8537b841c9ed3856ecb2d92a3c6fb87e03a3bc2
jamisoncozart <jamisoncozart@gmail.com>
2020-02-18 10:48:39 -0800

    Update README.md instructions

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ gameScene.create = function() {
     this.anims.create({
         key: 'walk',
         frames: this.anims.generateFrameNames('player', {prefix: 'p1_walk', start: 1, end: 11, zeroPad: 2}),
-        frameRate: 60,
+        frameRate: 10,
         repeat: -1
     });
     // idle with only one frame, so repeat is not neaded
